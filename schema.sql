217eb3dc751673d36520a143aae62aef66861041
Luz Maria <luzmariacabral80@gmail.com>
2021-05-01 15:58:27 -0700

    schema update-retiremnt

--- schema.sql
+++ schema.sql
@@ -41,16 +41,21 @@ CREATE TABLE titles (
 	from_date DATE  NOT  NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 
-CREATE TABLE Dept_emp (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 SELECT * FROM departments;
+SELECT * FROM titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
