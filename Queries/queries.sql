b09b948b0150108b20e441558500992c396b665c
Luz Maria <luzmariacabral80@gmail.com>
2021-05-02 10:49:19 -0700

    more lists

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -143,3 +143,29 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+-- List of Department retirees in sales
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name 
+INTO sales_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name='Sales';
+
+--List of Dept retirees in sales and develpment
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name 
+INTO SDretiree_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN('Sales','Development');
