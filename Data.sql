c95e061c79fd4aca83b8b8ef3b92c976edba6c39
Luz Maria <luzmariacabral80@gmail.com>
2021-05-02 18:00:12 -0700

    Deliverable 2

--- Data.sql
+++ Data.sql
@@ -204,3 +204,21 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT DESC;
 
+--Employees eligible to mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER by e.emp_no;
+
--- Queries/Employee_Database_challenege.sql/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Database_challenege.sql/Employee_Challenge_starter_code.sql
@@ -7,3 +7,45 @@ ______
 INTO nameyourtable
 FROM _______
 ORDER BY _____, _____ DESC;
+
+-- Creating table of retiring employees by title
+SELECT e.emp_no, 
+	e.first_name, 
+e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+last_name,
+first_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- COunt of retirees, their title
+SELECT COUNT(title), 
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
